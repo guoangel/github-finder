ca64e8b3d3ed89060338be569eac2cd8c01527b5
angel <fsguoangel@hotmail.com>
2022-01-09 14:52:34 +0800

    user profile and alerts

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Footer from './components/layout/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import User from './pages/User'
+import Alert from './components/layout/Alert'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
     <Router>
-      <div className='flex flex-col justify-between h-screen'>
+      <div className='flex flex-col justify-between h-screen bg-slate-600'>
         <Navbar />
 
         <main className='container mx-auto px-3 pb-12'>
+        <Alert />
         <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/user/:login' element={<User />} />
         <Route path='/notfound' element={<NotFound />} />
         <Route path='*' element={<NotFound />} />
         </Routes>
@@ -24,6 +30,7 @@ function App() {
         <Footer />
       </div>
     </Router>
+    </AlertProvider>
     </GithubProvider>
   )
 }
