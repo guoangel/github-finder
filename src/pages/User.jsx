c4b0792b24e33c58ca5d214ace20cce7beaa8e7f
angel <fsguoangel@hotmail.com>
2022-01-09 15:22:22 +0800

    completed

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,19 +1,26 @@
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { useEffect, useContext } from 'react'
+import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
 import GithubContext from '../context/github/GithubContext'
-import { useParams } from 'react-router-dom'
+import { getUserAndRepos } from '../context/github/GithubActions'
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } =
-    useContext(GithubContext)
-    const params = useParams()
-    useEffect(() => {
-      getUser(params.login)
-      getUserRepos(params.login)
-    }, [])
+  const { user, loading, repos, dispatch } = useContext(GithubContext)
+
+  const params = useParams()
+
+  useEffect(() => {
+    dispatch({ type: 'SET_LOADING' })
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login)
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData })
+    }
+
+    getUserData()
+  }, [dispatch, params.login])
 
   const {
     name,
